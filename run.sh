8a0e1a438bf6731f6bca033addbe28882aed3260
snehkothari28 <sneh.kothari28@gmail.com>
2022-12-15 02:41:34 +0530

    Modified props

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-mvn clean spring-boot:start -Dspring-boot.run.arguments="--spring.profiles.active=server"
+#mvn clean spring-boot:start -Dspring-boot.run.arguments="--spring.profiles.active=server"
+mvn clean package
+java -jar target/webauth-1.0.jar --spring.profiles.active=server

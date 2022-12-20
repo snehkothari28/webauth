0d541b0cafc4a19b69bd075e110bb038fbfe027b
snehkothari28 <sneh.kothari28@gmail.com>
2022-12-21 03:34:27 +0530

    Adding shell scripts for automation

--- run.sh
+++ run.sh
@@ -9,3 +9,5 @@ timestamp=$(date "+%Y%m%d%H%M%S")
 [ ! -d server-logs/"$timestamp" ] && mkdir server-logs/"$timestamp"
 nohup java -jar target/webauth-1.0.jar --spring.profiles.active=server 1>./server-logs/"$timestamp"/stdout.txt 2>./server-logs/"$timestamp"/stderr.txt &
 echo $! > save_pid.txt
+
+echo "Running webauth server and logging at server-logs/${timestamp}"

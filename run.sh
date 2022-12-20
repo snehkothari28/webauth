ab9d5fc516d9d21e2df76c273de920a6e083032c
snehkothari28 <sneh.kothari28@gmail.com>
2022-12-21 03:13:01 +0530

    Adding shell scripts for automation

--- run.sh
+++ run.sh
@@ -4,5 +4,7 @@ mvn clean package
 if test -f nohup.out; then
   rm nohup.out
 fi
-nohup java -jar target/webauth-1.0.jar --spring.profiles.active=server 1>./server-logs/stdout.txt 2>./server-logs/stderr.txt &
+timestamp=$(date "+%Y%m%d%H%M%S")
+mkdir server-logs/timestamp
+nohup java -jar target/webauth-1.0.jar --spring.profiles.active=server 1>./server-logs/timestamp/stdout.txt 2>./server-logs/timestamp/stderr.txt &
 echo $! > save_pid.txt

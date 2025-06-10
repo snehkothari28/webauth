9152890469f68f2010c6b37789c758d798a9f6d9
Sneh Kothari <sneh.kothari28@gmail.com>
2025-06-10 13:55:46 +0530

    Add configuration to enable/disable backup service

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ fi
 timestamp=$(date "+%Y%m%d%H%M%S")
 [ ! -d server-logs ] && mkdir server-logs
 [ ! -d server-logs/"$timestamp" ] && mkdir server-logs/"$timestamp"
-nohup java -jar target/webauth-1.0.jar --spring.profiles.active=server >./server-logs/"$timestamp"/log.txt 2>&1 &
+nohup java -Xmx512m -jar target/webauth-1.0.jar --spring.profiles.active=server >./server-logs/"$timestamp"/log.txt 2>&1 &
 echo $! >save_pid.txt
 
 echo "Running webauth server and logging at server-logs/${timestamp}"
